74fbccbbc88a1d1ebfa2559cae3f511ce2d06538
Marco Köpcke <marco.koepcke@tudock.de>
2023-01-19 12:56:08 +0100

    PHP 8.2

--- php/builder/build.sh
+++ php/builder/build.sh
@@ -11,7 +11,7 @@ for goal in */*/*; do
     variant="${parts[2]}"
 
     # GD options for various PHP versions
-    if [[ "7.4" == "$version" ||  "8.0" == "$version" ]]
+    if [[ "7.4" == "$version" ||  "8.0" == "$version" ||  "8.1" == "$version" ||  "8.2" == "$version" ]]
     then
         GD_OPTIONS="--with-freetype --with-jpeg"
     else
