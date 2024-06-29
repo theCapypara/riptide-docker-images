206d0513581082a7598936403a2d135efeb3d861
Marco "Capypara" Köpcke <hello@capypara.de>
2024-06-29 16:55:28 +0200

    bump Sphinx

--- sphinx/sphinx-start-docker.sh
+++ sphinx/sphinx-start-docker.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+set -e
+python3 -m venv --system-site-packages /tmp/riptide-venv
+. /tmp/riptide-venv/bin/activate
 if [ ! -z "$REQUIREMENTS_FILE" ]; then
-    pip install --user -r"$REQUIREMENTS_FILE"
+    pip install -I -r"$REQUIREMENTS_FILE"
 fi
 sphinx-autobuild  --ignore "_riptide/*" --host 0.0.0.0 --port 8000 "$SPHINX_SOURCE" "$SPHINX_BUILD"/html
