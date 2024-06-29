eb8d90c2ba6afcf581cf374a0c711e7903fe05d2
Marco "Capypara" Köpcke <hello@capypara.de>
2024-06-29 17:10:47 +0200

    Sphinx: Fix package scope

--- sphinx/sphinx-start-docker.sh
+++ sphinx/sphinx-start-docker.sh
@@ -5,4 +5,4 @@ python3 -m venv --system-site-packages /tmp/riptide-venv
 if [ ! -z "$REQUIREMENTS_FILE" ]; then
     pip install -I -r"$REQUIREMENTS_FILE"
 fi
-sphinx-autobuild  --ignore "_riptide/*" --host 0.0.0.0 --port 8000 "$SPHINX_SOURCE" "$SPHINX_BUILD"/html
+python -m sphinx_autobuild.__main__  --ignore "_riptide/*" --host 0.0.0.0 --port 8000 "$SPHINX_SOURCE" "$SPHINX_BUILD"/html
